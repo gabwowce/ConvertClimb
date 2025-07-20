8f084583ce310b0ec2191afb37b8b2937e5472d0
gabwowce <gt1230112301@gmail.com>
2025-07-20 14:13:05 +0300

    SENS=1

--- src/components/config/sliderConfig.ts
+++ src/components/config/sliderConfig.ts
@@ -1,7 +1,7 @@
 export const STEPS = 33;
 export const THUMB = 53;
 export const R = THUMB / 2;
-export const SENS = 0.5; // 0.5 = 2× lėčiau
+export const SENS = 1; // 0.5 = 2× lėčiau
 export const TOP_PAD = 80;
 export const BOTTOM_PAD = 80;
 export const STEP_PCT = 1 / (STEPS - 1);
